429d8119039a790b5bbb13deaae0f9bde1f959f3
bckim25 <bckim@nuga.kr>
2024-05-21 00:30:46 +0900

    feat : Problems with current work

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
+
 
 function App() {
-  let tabContent = "Please click a button"; 
-  const [ selectedTopic, setSelectedTopic ] = useState('please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
 function handleSelect(selectedButton) {
   //selectedButton => 'components','jsx','props','state'
@@ -18,6 +19,21 @@ function handleSelect(selectedButton) {
 }
 
 console.log("APP COMPONENT EXECUTING");
+let tabContent = <p>Please select a topic.</p>;
+
+if (selectedTopic) {
+  tabContent = (
+        <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+  );
+}
 
   return (
     <div>
@@ -39,12 +55,19 @@ console.log("APP COMPONENT EXECUTING");
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} 
+            onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'jsx'}
+            onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'props'}
+            onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton 
+            isSelected={selectedTopic === 'state'}
+            onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+            {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,11 +1,9 @@
-export default function TabButton({ children, onSelect }) {
-    // function handleClick() {
-    //     console.log('Hello World!');
-    // }
+export default function TabButton({ children, onSelect, isSelected }) {
+
     console.log("TABBUTTON COMPONENT EXECUTING");
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 }
